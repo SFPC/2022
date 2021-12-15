87148450344b6640b4a77133ddcc078cdbfad44b
Zainab Aliyu <zoaliyu@gmail.com>
2021-12-15 15:31:12 -0500

    mobile height + color themes

--- outside.js
+++ outside.js
@@ -1,5 +1,15 @@
 $(window).height();
 
+function resetHeight(){
+    // reset the body height to that of the inner browser
+    document.body.style.height = window.innerHeight + "px";
+}
+// reset the height whenever the window's resized
+window.addEventListener("resize", resetHeight);
+// called to initially set the height.
+resetHeight();
+
+
 
 
 const modeStyles = document.getElementById('mode')
