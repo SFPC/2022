46f7854ffa8e67e80194a144ad4f350195e530ca
Zainab Aliyu <zoaliyu@gmail.com>
2021-12-16 02:07:03 -0500

    slideshow

--- carousel.js
+++ carousel.js
@@ -1,5 +1,7 @@
 const slider = document.querySelector('.items');
+const slider2 = document.querySelector('.items2');
 let isDown = false;
+let isDown2 = false;
 let startX;
 let scrollLeft;
 
@@ -23,5 +25,29 @@ slider.addEventListener('mousemove', (e) => {
   const x = e.pageX - slider.offsetLeft;
   const walk = (x - startX) * 3; //scroll-fast
   slider.scrollLeft = scrollLeft - walk;
+  console.log(walk);
+});
+
+
+slider2.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider2.classList.add('active');
+  startX = e.pageX - slider2.offsetLeft;
+  scrollLeft = slider2.scrollLeft;
+});
+slider2.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider2.classList.remove('active');
+});
+slider2.addEventListener('mouseup', () => {
+  isDown = false;
+  slider2.classList.remove('active');
+});
+slider2.addEventListener('mousemove', (e) => {
+  if(!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider2.offsetLeft;
+  const walk = (x - startX) * 3; //scroll-fast
+  slider2.scrollLeft = scrollLeft - walk;
   console.log(walk);
 });
