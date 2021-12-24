cbcbe668776bdd064b49528b5fbcdd4be5e58c36
Zainab Aliyu <zoaliyu@gmail.com>
2021-12-24 13:10:55 -0500

    overlay size

--- outside.js
+++ outside.js
@@ -406,7 +406,9 @@ $(document).ready(function() {
           $(".mobile-menu").click(function() {
               // $(".shell").addClass('shell-halfsize').removeClass('shell-fullsize');
               $(".shell").toggleClass('shell-halfsize');
-              $(".shell").toggleClass('full-halfsize');
+              $(".shell").toggleClass('shell-fullsize');
+              $(".crt-overlay").toggleClass('crt-overlay-halfsize');
+              $(".crt-overlay").toggleClass('crt-overlay--fullsize');
               $(".mobile-menu-content").toggleClass('mobile-menu-content-show');
 
               if ($(".mobile-menu").text() == "Menu") {
